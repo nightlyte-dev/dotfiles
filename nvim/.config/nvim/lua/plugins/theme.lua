7ec09cdde95ba03a3e05d5d2e5284c40b72ac137
Nightlyte <nightlyte.io@gmail.com>
2026-01-11 19:33:58 -0600

    Fixed accidental symlink

--- nvim/.config/nvim/lua/plugins/theme.lua
+++ nvim/.config/nvim/lua/plugins/theme.lua
@@ -1,1 +1,20 @@
-/home/nightlyte/.config/omarchy/current/theme/neovim.lua
+return {
+	{
+		"olimorris/onedarkpro.nvim",
+		priority = 1000,
+		config = function()
+			require("onedarkpro").setup({
+				theme = "onedark",
+				highlights = {
+					-- Match gutter background to main background
+					LineNr = { fg = "#5c6370", bg = "bg" },
+
+					TabLineFill = { bg = "bg" },
+					TabLine = { bg = "bg" },
+					TabLineSel = { bg = "#282c34" }, -- active tab
+				},
+			})
+			vim.cmd("colorscheme onedark")
+		end,
+	},
+}
