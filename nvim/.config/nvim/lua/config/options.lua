8e80429628e999754b53aaf508e529a09a193ee9
Nightlyte <nightlyte.io@gmail.com>
2026-01-19 06:59:35 -0600

    Fixed neovim config

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -2,4 +2,5 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.opt.relativenumber = true
-vim.g.lazyvim_ruby_lsp = "ruby_lsp"
+vim.opt.wrap = true
+-- vim.g.lazyvim_ruby_lsp = "ruby_lsp"
